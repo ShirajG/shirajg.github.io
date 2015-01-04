81af6e876710b8b8e3f76680e36c9f8fbd99b1af
Shiraj Ganguly <shirajganguly@gmail.com>
2015-01-04 12:49:07 -0500

    Use AJAX requests to load posts. Also added an animation to the header

--- js/script.js
+++ js/script.js
@@ -1,48 +1,55 @@
 $(document).ready(function(){
-
-var techMenu = $(".menus li:first-child");
-var cultureMenu = $(".menus li:nth-child(2)");
-var about = $(".menus li:last-child");
-var technical = $('#technical');
-var cultural = $('#cultural');
-var portrait = $('.portrait');
-technical.hide();
-cultural.hide();
-
-techMenu.click(function (event) {
-  event.preventDefault();
-  technical.slideToggle('fast');
-  cultural.hide();
-  techMenu.addClass('jquery');
-  cultureMenu.removeClass('jquery');
-  about.removeClass('jquery');
-});
-cultureMenu.click(function (event) {
-  event.preventDefault();
-  technical.slideToggle('fast');
-  cultural.slideToggle('fast');
-  technical.hide();
-  cultureMenu.addClass('jquery');
-  techMenu.removeClass('jquery');
-  about.removeClass('jquery');
-});
-about.click(function () {
+  var techMenu = $(".menus li:first-child"),
+  cultureMenu = $(".menus li:nth-child(2)"),
+  about = $(".menus li:last-child"),
+  technical = $('#technical'),
+  cultural = $('#cultural'),
+  portrait = $('.portrait'),
+  links = $('#technical a, #cultural a, #about')
   technical.hide();
   cultural.hide();
-  about.addClass('jquery');
-  techMenu.removeClass('jquery');
-  cultureMenu.removeClass('jquery');
-});
-technical.click(function (){
-  technical.slideUp('fast');
-  $(window).scrollTop($(document).height());
-});
-cultural.click(function (){
-  cultural.slideUp('fast');
-  $(window).scrollTop($(document).height());
-});
 
+  techMenu.click(function (event) {
+    event.preventDefault();
+    technical.slideToggle('fast');
+    cultural.hide();
+    techMenu.addClass('jquery');
+    cultureMenu.removeClass('jquery');
+    about.removeClass('jquery');
+  });
+  cultureMenu.click(function (event) {
+    event.preventDefault();
+    technical.slideToggle('fast');
+    cultural.slideToggle('fast');
+    technical.hide();
+    cultureMenu.addClass('jquery');
+    techMenu.removeClass('jquery');
+    about.removeClass('jquery');
+  });
+  about.click(function () {
+    technical.hide();
+    cultural.hide();
+    about.addClass('jquery');
+    techMenu.removeClass('jquery');
+    cultureMenu.removeClass('jquery');
+  });
+  technical.click(function (){
+    technical.slideUp('fast');
+    // $(window).scrollTop($(document).height());
+  });
+  cultural.click(function (){
+    cultural.slideUp('fast');
+    // $(window).scrollTop($(document).height());
+  });
+  links.click(function(event){
+    event.preventDefault()
+    console.log(event)
+    $.get(event.target.href, function(response){
+      newPost = $(response).filter('#post')
+      $('#post').replaceWith(newPost)
+      // console.log($rawResponse)
+      // debugger
 
-
-
+    })
+  })
 });
